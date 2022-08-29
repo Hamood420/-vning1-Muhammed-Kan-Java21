2b1bf7e3bf57999ea79c8a09c93ed8aa5ec23d43
Hamood420 <91257348+Hamood420@users.noreply.github.com>
2022-08-29 13:32:13 +0200

    Add files via upload

---
+++ övning1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class övning1 {
+
+	public static void main(String[] args) {
+		 final String PASSWORD = "Test";
+		    Scanner sc = new Scanner(System.in);
+		    int attempts = 3;
+		    String password = "";
+		    while (attempts-- > 0 && !PASSWORD.equals(password)) 
+		    {
+		        System.out.print("Enter your password: ");
+		        password = sc.nextLine();
+		        if (password.equals(PASSWORD)) 
+		            System.out.println("Welcome");
+		        else 
+		            System.out.println("Incorrect. Number of attempts remaining: " + attempts);        
+		    }
+		}
+}
+
