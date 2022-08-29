07fd3cb2dbbfff38a4d42a292be324257402f5ac
Hamood420 <91257348+Hamood420@users.noreply.github.com>
2022-08-29 13:22:29 +0200

    Add files via upload

---
+++ src/demo.java
@@ -0,0 +1,27 @@
+
+public class demo {
+	
+	// ctrl + space main sysout mm
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[1]);
+
+		boolean flagga = true;
+		byte bit8 = 127;
+		int siffra = -5;
+		
+		float decimaltal = 0.55f;
+		short kort = 123;
+		char symbol = '@';
+		String Text = "hello";
+		long stortHeltal = 12345678L;
+		double stortDecimaltal = 012345D;
+	
+		//Omslagsklasser / wrapperclass
+		
+		System.out.println(Integer.parseInt("123"));
+		System.out.println(Boolean.parseBoolean("FALSE"));
+
+		
+	}
+}
